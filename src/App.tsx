c2c05c5e6303ef1e3fdb60776184ad01b8ac24c7
Matthew Song <songww1997@naver.com>
2024-04-06 16:30:36 +0900

    feat: 홈 레이아웃 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import { Link } from "react-router-dom";
 import "./App.css";
 import Router from "./components/Router";
 
 function App() {
   return (
     <>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/posts">Post</Link>
-        <Link to="/posts:id">Detail</Link>
-      </div>
       <Router />
     </>
   );
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,23 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <header>
+        <div>
+          <Link to="/posts/new">글쓰기</Link>
+          <Link to="/posts">게시글</Link>
+          <Link to="/profile">프로필</Link>
+        </div>
+      </header>
+      <div className="post-list"></div>
+      <footer>
+        <div>Menu 1</div>
+        <div>Menu 1</div>
+        <div>Menu 1</div>
+      </footer>
+    </div>
+  );
 };
 
 export default Home;
