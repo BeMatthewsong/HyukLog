a36a93d9e2b75ad5c299e879a4dc597448db4ba8
Matthew Song <songww1997@naver.com>
2024-04-06 16:18:31 +0900

    feat: 페이지 컴포넌트초기 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { Link, Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./App.css";
+import Router from "./components/Router";
 
 function App() {
   return (
@@ -9,15 +10,7 @@ function App() {
         <Link to="/posts">Post</Link>
         <Link to="/posts:id">Detail</Link>
       </div>
-      <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/posts" element={<h1>Post List</h1>} />
-        <Route path="/posts:id" element={<h1>Post Detail</h1>} />
-        <Route path="/posts/new" element={<h1>New</h1>} />
-        <Route path="/posts/edit:id" element={<h1>New</h1>} />
-        <Route path="/profile" element={<h1>New</h1>} />
-        <Route path="/*" element={<h1>New</h1>} />
-      </Routes>
+      <Router />
     </>
   );
 }
---
+++ src/components/Router.tsx
@@ -0,0 +1,23 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import PostDetail from "../pages/detail";
+import PostEdit from "../pages/edit";
+import Home from "../pages/home";
+import PostNew from "../pages/new";
+import Posts from "../pages/posts";
+import Profile from "../pages/profile";
+
+const Router = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/posts" element={<Posts />} />
+      <Route path="/posts:id" element={<PostDetail />} />
+      <Route path="/posts/new" element={<PostNew />} />
+      <Route path="/posts/edit:id" element={<PostEdit />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/*" element={<Navigate replace to="/" />} />
+    </Routes>
+  );
+};
+
+export default Router;
