e18c07df4806abf81c161184a5c6952bdf43d030
Matthew Song <songww1997@naver.com>
2024-04-08 17:30:10 +0900

    refactor: 슬라이드 컴포넌트 리팩토링

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,36 +1,39 @@
 import { useState } from "react";
-import img1 from "src/assets/벚꽃1.jpg";
-import img2 from "src/assets/벚꽃2.jpg";
-import img3 from "src/assets/벚꽃3.jpg";
+import SakuraImg from "src/assets/벚꽃1.jpg";
+import CableCarImg from "src/assets/벚꽃2.jpg";
+import NamsanLibraryImg from "src/assets/벚꽃3.jpg";
 import SlideImage from "./SlideImage";
 
 const Carousel = () => {
   const [activeImage, setActiveImage] = useState(1);
 
+  const imageDatas = [
+    {
+      alt: "남산 벚꽃",
+      src: SakuraImg,
+    },
+    {
+      alt: "남산 케이블",
+      src: CableCarImg,
+    },
+    {
+      alt: "남산 도서관 옆",
+      src: NamsanLibraryImg,
+    },
+  ];
+
   return (
     <div className="carousel">
       <ul className="carousel__slides">
-        <SlideImage
-          imageSrc={img1}
-          alt={"남산 벚꽃"}
-          activeImage={activeImage}
-          imageNumber={0}
-          setActiveImage={setActiveImage}
-        />
-        <SlideImage
-          imageSrc={img2}
-          alt={"남산 케이블"}
-          activeImage={activeImage}
-          imageNumber={1}
-          setActiveImage={setActiveImage}
-        />
-        <SlideImage
-          imageSrc={img3}
-          alt={"남산 도서관 옆"}
-          activeImage={activeImage}
-          imageNumber={2}
-          setActiveImage={setActiveImage}
-        />
+        {imageDatas.map((e, index) => (
+          <SlideImage
+            imageSrc={e.src}
+            alt={e.alt}
+            activeImage={activeImage}
+            imageNumber={index}
+            setActiveImage={setActiveImage}
+          />
+        ))}
         <div className="carousel__dots">
           {[...Array(3)].map((_, index) => (
             <label
